059bcf0f0bc44900a3b2e7c593cb1bdf268dc115
nodirbek75 <nodirbek7077@gmail.com>
2022-05-09 19:21:09 +0900

    added NAV002

--- src/v1/components/NAV002.tsx
+++ src/v1/components/NAV002.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import styled from 'styled-components/native';
+import {boolean, string} from 'yargs';
 
-const NAV002 = () => {
-  return <div>NAV002</div>;
+type Props = {
+  id: string;
+  screen: string;
+  isPreserveGlobalSave: boolean;
+  buttonText: string;
+  isEnabled: boolean;
+};
+
+const NAV002: React.FC<Props> = ({buttonText}) => {
+  return (
+    <Wrapper>
+      <Text>{buttonText}</Text>
+    </Wrapper>
+  );
 };
 
 export default NAV002;
+
+const Wrapper = styled.TouchableOpacity``;
+
+const Text = styled.Text``;
