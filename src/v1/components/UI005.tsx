f1bb297027a2233c87cbf5fd233e39dbefb573bf
johnny-washswat <johnny@washswat.com>
2022-05-10 16:28:47 +0900

    common components

--- src/v1/components/UI005.tsx
+++ src/v1/components/UI005.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import Spacer from './common/Spacer';
 
 type Props = {
   id: string;
@@ -10,15 +11,6 @@ type Props = {
   itemList: string;
   actions: any;
   isChecked: boolean;
-};
-
-type SpacerProps = {
-  height?: number;
-  width?: number;
-};
-
-const Spacer: React.FC<SpacerProps> = ({height, width}) => {
-  return <ViewSpacer height={height} width={width} />;
 };
 
 const UI005: React.FC<Props> = ({
@@ -109,9 +101,4 @@ const ItemList = styled.Text`
   font-size: 14px;
   text-align: right;
   color: #5d6166;
-`;
-
-const ViewSpacer = styled.View<SpacerProps>`
-  height: ${props => props.height || 0}px;
-  width: ${props => props.width || 0}px;
 `;
